4132a24b4d2651f18ebc9caf07d80b5ef757eaa4
creativeartbd <creativeartbd@gmail.com>
2020-10-17 12:51:04 +0600

    Update code

--- index.php
+++ index.php
@@ -25,58 +25,60 @@ get_header();
 	</div>
 </div>
 
-<div class="container">
-	<div class="row">
-		<div class="col-md-12">
-			<main id="primary" class="site-main speedway-blog">
-				<?php
-				if ( have_posts() ) :
-					if ( is_home() && ! is_front_page() ) :
-						?>
-						<header>
-							<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-						</header>
-						<?php
-					endif;
-					/* Start the Loop */
-					?>
-					<div class="row">
+<div class="container-fluid">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<main id="primary" class="site-main speedway-blog">
 					<?php
-					while ( have_posts() ) :
-						the_post();
-						/*
-						* Include the Post-Type-specific template for the content.
-						* If you want to override this in a child theme, then include a file
-						* called content-___.php (where ___ is the Post Type name) and that will be used instead.
-						*/
-						?>					
-						<div class="col-md-6">	
-							<div class="speedway-article">
-								<?php
-								get_template_part( 'template-parts/content', 'all' );
-								?>
-							</div>								
+					if ( have_posts() ) :
+						if ( is_home() && ! is_front_page() ) :
+							?>
+							<header>
+								<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+							</header>
+							<?php
+						endif;
+						/* Start the Loop */
+						?>
+						<div class="row load-more-post">
+						<?php
+						while ( have_posts() ) :
+							the_post();
+							/*
+							* Include the Post-Type-specific template for the content.
+							* If you want to override this in a child theme, then include a file
+							* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+							*/
+							get_template_part( 'template-parts/content', 'all' );
+						endwhile;
+						?>
 						</div>
 						<?php
-					endwhile;
+						//the_posts_navigation();
+					else :
+						get_template_part( 'template-parts/content', 'none' );
+					endif;
 					?>
-					</div>
-					<?php
-					the_posts_navigation();
-				else :
-					get_template_part( 'template-parts/content', 'none' );
-				endif;
+				</main><!-- #main -->
+			</div>
+			
+			<div class="col-xs-6 col-xs-offset-4">
+				<?php
+				global $wp_query; // you can remove this line if everything works for you
+				
+				// don't display the button if there are not enough posts
+				if (  $wp_query->max_num_pages > 1 )
+					echo '<div class="misha_loadmore text-center">More posts</div>'; // you can use <a> as well
 				?>
-			</main><!-- #main -->
-		</div> 	
-		<!-- col-md-12 end here -->
+			</div> 	
+			<!-- col-md-12 end here -->
+		</div>
+		<!-- row end here -->
 	</div>
-	<!-- row end here -->
+	<!-- Container end here -->
 </div>
-<!-- Container end here -->
 
-
-<div class="bg"></div>
 <div class="container-fluid promo">	
 	<div class="container">
 		<div class="row">
--- template-parts/content-all.php
+++ template-parts/content-all.php
@@ -8,83 +8,96 @@
  */
 
 ?>
+<div class="col-sm-6 col-md-6">	
+	<div class="speedway-article">
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
-	<div class="post-category-date-wrapper">
-		<div class="post-category">
-			<?php 
-			$speed_post_category = get_the_category(); 			
-			foreach( $speed_post_category  as $category ) {
-				$category_link = get_category_link( $category->term_id );
-				$category_name = $category->name;
-				echo "<a href='{$category_link}'>{$category_name}</a>";
-			}			
-			?>
-		</div>
-		<div class="post-date">
-			<?php echo get_the_date(); ?>
-		</div>
-	</div>
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			
+			<div class="row">
+				<div class="col-sm-12 col-md-6">
+					<div class="post-category">
+						<?php 
+						$speed_post_category = get_the_category(); 			
+						foreach( $speed_post_category  as $category ) {
+							$category_link = get_category_link( $category->term_id );
+							$category_name = $category->name;
+							echo "<a href='{$category_link}'>{$category_name}</a>";
+						}			
+						?>
+					</div>
+				</div>
+				<div class="col-sm-12 col-md-6">
+					<div class="post-date">
+						<?php echo get_the_date(); ?>
+					</div>
+				</div>
+			</div>
+			<!-- <div class="post-category-date-wrapper">
+				
+				
+			</div> -->
 
-	<?php speedway_post_thumbnail(); ?>
+			<?php speedway_post_thumbnail(); ?>
 
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			//the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			echo '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . mb_strimwidth(get_the_title(), 0, 50, '...') . '</a></h2>';
-		endif;
+			<header class="entry-header">
+				<?php
+				if ( is_singular() ) :
+					the_title( '<h1 class="entry-title">', '</h1>' );
+				else :
+					//the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+					echo '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . mb_strimwidth(get_the_title(), 0, 50, '...') . '</a></h2>';
+				endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>			
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+				if ( 'post' === get_post_type() ) :
+					?>			
+				<?php endif; ?>
+			</header><!-- .entry-header -->
 
-	
+			
 
-	<div class="entry-content">
-		<?php
-        $string = strip_tags( get_the_excerpt() );
-		if (strlen($string) > 100) {
+			<div class="entry-content">
+				<?php
+				$string = strip_tags( get_the_excerpt() );
+				if (strlen($string) > 100) {
 
-			// truncate string
-			$stringCut = substr($string, 0, 100);
-			$endPoint = strrpos($stringCut, ' ');
+					// truncate string
+					$stringCut = substr($string, 0, 100);
+					$endPoint = strrpos($stringCut, ' ');
 
-			//if the string doesn't contain any space then it will cut without word basis.
-			$string = $endPoint? substr($stringCut, 0, $endPoint) : substr($stringCut, 0);
-			//$string .= '... <a href="/this/story">Read More</a>';
-		}
-		echo $string;
+					//if the string doesn't contain any space then it will cut without word basis.
+					$string = $endPoint? substr($stringCut, 0, $endPoint) : substr($stringCut, 0);
+					//$string .= '... <a href="/this/story">Read More</a>';
+				}
+				echo $string;
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'speedway' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php edit_post_link(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Edit <span class="screen-reader-text">%s</span>', 'speedway' ),
+				wp_link_pages(
 					array(
-						'span' => array(
-							'class' => array(),
-						),
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'speedway' ),
+						'after'  => '</div>',
 					)
-				),
-				wp_kses_post( get_the_title() )
-			),
-			'<span class="edit-link">',
-			'</span>'
-		); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+				);
+				?>
+			</div><!-- .entry-content -->
+
+			<footer class="entry-footer">
+				<?php edit_post_link(
+					sprintf(
+						wp_kses(
+							/* translators: %s: Name of current post. Only visible to screen readers */
+							__( 'Edit <span class="screen-reader-text">%s</span>', 'speedway' ),
+							array(
+								'span' => array(
+									'class' => array(),
+								),
+							)
+						),
+						wp_kses_post( get_the_title() )
+					),
+					'<span class="edit-link">',
+					'</span>'
+				); ?>
+			</footer><!-- .entry-footer -->
+		</article><!-- #post-<?php the_ID(); ?> -->
+
+	</div>
+</div>
