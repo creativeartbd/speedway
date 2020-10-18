92663027e25817e8de410c8b87305a03a5ab816f
creativeartbd <creativeartbd@gmail.com>
2020-10-18 10:42:42 +0600

    Update Style

--- js/myloadmore.js
+++ js/myloadmore.js
@@ -1,4 +1,7 @@
 jQuery(function($){ // use jQuery code inside this to avoid "$ is not defined" error
+
+	document.getElementById("bdt-advanced-gmap-3401a49address").placeholder = "Postcode / Suburb";
+
 	$('.misha_loadmore').click(function(){
  
 		var button = $(this),
